efd2ed3ede8d8b75e41705b5a4284ab36494bf8a
Alex Feyerke <kontakt@momoi.de>
2013-05-28 16:24:36 +0200

    Added app-wide resize event

--- www/assets/app.js
+++ www/assets/app.js
@@ -2,7 +2,7 @@
 var HOODIE_API_URL;
 if (/github\.io$/.test(location.hostname)) {
   HOODIE_API_URL = 'http://hoodie-mapchat.jit.su/_api';
-} 
+}
 
 // Init Hoodie and tell it where it's API is
 var hoodie = new Hoodie(HOODIE_API_URL)
@@ -14,17 +14,25 @@ bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }
 
   var $document = $(document)
 
-  // 
+  //
   // gets executed when dom ready
-  // 
+  //
   function init (event) {
 
     // FastClick makes mobile devices respond quicker to touch events
     new FastClick(document.body);
 
+    var throttledResize = _.throttle(sendResizeEvent, 300);
+    $(window).on('resize', function(event){
+      throttledResize();
+    })
+
     $.event.trigger('app:ready')
   }
   $document.ready( init )
-  
+
+  function sendResizeEvent() {
+    $.event.trigger('app:resize')
+  }
 
 })(jQuery);
